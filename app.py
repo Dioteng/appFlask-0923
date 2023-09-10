1c8d62ebc1fe60c56a74569616b415d49314266b
Dioteng <jonaemazis@gmail.com>
2023-09-10 21:23:02 +0800

    Flask + SQLAlchemy MySQL

--- app.py
+++ app.py
@@ -1,33 +1,72 @@
-from flask import Flask, render_template
+#app.py
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
+  
 app = Flask(__name__)
 app.secret_key = "my-secret-key"
-
-#SqlAlchemy Database Configuration With Mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/ride4cast'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-#Creating model table for our CRUD database
-class Bus(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    bus_number = db.Column(db.String(100))
-    plate_number = db.Column(db.String(100))
-    capacity = db.Column(db.Integer)
-    date_added = db.Column(db.DateTime, default = datetime.now)
   
-    def __init__(self, bus_number, plate_number, capacity, date_added):
-        self.bus_number = bus_number
-        self.plate_number = plate_number
-        self.capacity = capacity
-        self.date_added = date_added
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
+#SqlAlchemy Database Configuration With Mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/ride4castdb'
+                                        #mysql+pymysql://username:passwd@host/databasename 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  
+db = SQLAlchemy(app)
+  
+#Creating model table for our CRUD database
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+  
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+  
+#query on all our employee data
+@app.route('/')
+def Index():
+    all_data = Employee.query.all()
+    return render_template("index.html", employees = all_data)
+  
+#insert data to mysql database via html forms
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+  
+        my_data = Employee(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+  
+        flash("Employee Inserted Successfully")
+        return redirect(url_for('Index'))
+  
+#update employee
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Employee.query.get(request.form.get('id'))
+  
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+  
+        db.session.commit()
+        flash("Employee Updated Successfully")
+        return redirect(url_for('Index'))
+  
+#delete employee
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Employee.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Deleted Successfully")
+    return redirect(url_for('Index'))
+  
 if __name__ == "__main__":
     app.run(debug=True)
